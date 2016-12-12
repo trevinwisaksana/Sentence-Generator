e534de527bc7322e793a7c59474db83d2c4e2349
Trevin Wisaksana <trevinwisaksana@gmail.com>
2016-12-11 16:33:10 -0800

    Testing

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,11 +1,15 @@
 import Vapor
+import VaporPostgreSQL
 
-let drop = Droplet()
+let drop = Droplet(); try drop.addProvider(VaporPostgreSQL.Provider)
 
-drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+drop.get("version") { request in
+    if let db = drop.database?.driver as? PostgreSQLDriver {
+        let version = try db.raw("SELECT version()")
+        return try JSON(node: version)
+    } else {
+        return "No db connection"
+    }
 }
 
 drop.resource("posts", PostController())
